5ab7db046a16ab071893812a66af8ba8a27914db
Lee Smith <administrator@getaddress.io>
2021-12-28 16:43:11 +0000

    api-key replaced with env. variable

--- ts/test/api.test.ts
+++ ts/test/api.test.ts
@@ -1,14 +1,15 @@
 import {API} from "../API"
-import {getKey} from "./api-key"
+
+const apiKey:string = process.env.getaddress_apikey as string;
 
 test('autocomplete is success', async () => {
-    let api = new API(getKey());
+    let api = new API(apiKey);
     let result = await api.autocomplete('TR19 7AA');
     expect(result.isSuccess).toBe(true);
 });
 
 test('find is success', async () => {
-    let api = new API(getKey());
+    let api = new API(apiKey);
     let result = await api.find('TR19 7AA');
     expect(result.isSuccess).toBe(true);
 });
